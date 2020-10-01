98d4f14aa1aecac7bba7756f0cbdc800d6e8e17c
Akhil <ce.akhil@gmail.com>
2020-10-01 21:53:59 +0530

    Changed Positions

--- lib/camera_tap.dart
+++ lib/camera_tap.dart
@@ -12,25 +12,29 @@ class _CameraTapState extends State<CameraTap> {
     var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.grey[700],
-      body: Container(
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: PanFlareActor(
-            width: 135.0,
-            height: screenSize.height,
-            filename: 'assets/flr_files/slideoutMenu.flr',
-            openAnimation: 'open',
-            closeAnimation: 'close',
-            direction: ActorAdvancingDirection.RightToLeft,
-            threshold: 20.0,
-            reverseOnRelease:
-            true, // reverse's current animation when released and threshold not reaced
-            completeOnThresholdReached:
-            true, // plays the animation till the end when we reach threshold
-            activeAreas: [
-              RelativePanArea(
-                  area: Rect.fromLTWH(0, 0.7, 1.0, 0.3), debugArea: false)
-            ],
+      body: Center(
+        heightFactor: 20.0,
+        widthFactor: 10.0,
+        child: Container(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: PanFlareActor(
+              width: 135.0,
+              height: screenSize.height,
+              filename: 'assets/flr_files/slideoutMenu.flr',
+              openAnimation: 'open',
+              closeAnimation: 'close',
+              direction: ActorAdvancingDirection.RightToLeft,
+              threshold: 20.0,
+              reverseOnRelease:
+              true, // reverse's current animation when released and threshold not reaced
+              completeOnThresholdReached:
+              true, // plays the animation till the end when we reach threshold
+              activeAreas: [
+                RelativePanArea(
+                    area: Rect.fromLTWH(0, 0.7, 1.0, 0.3), debugArea: false)
+              ],
+            ),
           ),
         ),
       ),
