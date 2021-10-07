769c5f314ab02b57c218185ff2d3753197abf7f4
arvindsangwan <rvndsngwn@gmail.com>
2021-10-07 07:59:45 +0530

    responsive widget added and image tap

---
+++ lib/plus_tap.dart
@@ -0,0 +1,21 @@
+
+
+
+import 'image_tap.dart';
+
+double responsivePadding(double displayWidth) => displayWidth * 0.03;
+
+int responsiveNumCols(double displayWidth) {
+  if (displayWidth < pixelWidth1Column) {
+    return 1;
+  } else if (displayWidth < pixelWidth2Columns) {
+    return 2;
+  } else if (displayWidth < pixelWidth3Columns) {
+    return 3;
+  }
+  return 4;
+}
+
+double responsiveChildAspectRatio(double width, int colNumber) => width / (colNumber * 110);
+
+bool isDrawerFixed(double displayWidth) => displayWidth > pixelFixedDrawer;
